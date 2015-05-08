3842c107ed99434aa1625c461be6d11cd7bae1b9
MattiaPrimavera <sconqua@gmail.com>
2015-05-08 13:20:57 +0200

    retr request working

--- retr_req.c
+++ retr_req.c
@@ -2,29 +2,41 @@
 
 int PopRetr(char* requete, int desc, pop* response)
 {
-	printf("in PopTop function\n");
-	if(verifieSyntaxe(requete, "TOP") != 0) return -1;
+	printf("in PopRetr function\n");
+	if(verifieSyntaxe(requete, "RETR") != 0) return -1;
 
 	envoieServeur(requete, desc);
 
 	//lecture 1ere ligne
 	FILE* fdesc = fdopen(desc, "r");
 	char reponse[LINELENGTH];
-	printf("%s", reponse);
-	if(reponsePositive(fdesc, reponse) != 0) return -1;
+	printf("REPONSE:%s", reponse);
+	if(reponsePositive(fdesc, &reponse[0]) != 0) return -1;
 
 	//recuperation id message
 	int mexId = 0;
-	if(!sscanf(requete, "TOP %d 0\n", &mexId)){
+	if(!sscanf(requete, "RETR %d\n", &mexId)){
 		printf("ERRORE SCANSIONE ID\n");
 		return -1;
 	}
 	printf("MEX ID inserted: %d\n", mexId);
 	//if(mexId <= 0) return -1;
 
-	printf("ID messaggio1 : %d\n", response->listeMessages->id);
+	//printf("ID messaggio1 : %d\n", response->listeMessages->id);
 	//interpretation en-tetes
 	AnalyseEntetes(mexId, fdesc, response);
+
+	char* tmp = reponse;
+	//sauvegarde du corps
+	while(fgets(tmp, LINELENGTH, fdesc)){
+		printf("tmp:%s", tmp);
+	    if(!strncmp(".", tmp, strlen("."))){
+      		printf("\nfin CORPS ...\n");
+      		break;
+    	}
+    	tmp = tmp + strlen(tmp);
+	}
+	//printf("COOOOOOOOOORPS: \n\n%s\n", reponse);
 
     //sortie avec erreur
 	return -1;
